25bd80125cd0f3d5e989a8c9dfe96979d23eda39
Charles <cgdusek@nodehaven.com>
2019-10-31 16:36:53 -0500

    Updated name of account contract

--- contracts/HavenAccountContract.sol
+++ contracts/HavenAccountContract.sol
@@ -1,69 +1,204 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.12;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/ownership/Ownable.sol";
 
 contract UserContract is Ownable {
-    struct UserStruct {
-        string accountId;
-        string username;
+    
+    // The address of the Natural Rights Server
+    address public naturalRightsAddress;
+    
+    /// @dev Access modifier for NR-only functionality
+    modifier onlyNR() {
+        require(msg.sender == naturalRightsAddress);
+        _;
+    }
+    
+    struct privUserStruct {
+        string cryptPubKey;
         string encCryptPrivKey;
-        string encSigPrivKey;
+        string encSignPrivKey;
     }
-    mapping (address => UserStruct[])
-    mapping (address => uint256) listOfUsers; //address-> username hash
-    uint256[] listOfGunNicksIndex;
-    mapping (uint256 => string) listOfSocialUsernamesHashTranslations;
-    mapping (address => string) listOfSocialEncryptedKeys;
+    struct pubUserStruct {
+        string naturalRightsId;
+        string userName;
+        uint listPointer;
+    }
+    struct userNameTransfer {
+        address initiator;
+        string initExpectedUserName;
+        uint listPointer;
+        // bool paymentMade
+    }
 
+    mapping (address => privUserStruct) userPrivate; // private account backup
+    mapping (address => pubUserStruct) userPublic; // naturalRightsID to public identifiers
+    address[] public userList; // user index
+    mapping (uint256 => address) userNameOwner; // maps userName to Loom address
+    mapping (address => userNameTransfer) userNameTransferQueue; // Queues userName transfer from initiator (value) to comfirmer (key)
+    address[] public userNameTransferList; // List of username confirmers
+    mapping (uint256 => address) naturalRightsOwner; // maps NR ID to Loom address
+    
+    // Only contract owner may change Natural Rights Server
+    function setNaturalRightsServer(address _newNaturalRights) public onlyOwner returns(bool success) {
+        require(_naturalRights != address(0));
+        naturalRightsAddress = _newNaturalRights;
+        return true;
+    }
+    
+    // Only Natural Rights server may add users
+    function addUser(string _naturalRightsId, address _loomAddr) public onlyNR returns(bool success) {
+        naturalRightsIdHash = uint256(keccak256(_naturalRightsId));
+        naturalRightsOwner[naturalRightsIdHash] = _loomAddr;
+    }
+    
+    // Is user active
+    function isLoomUser(address _loomAddr) public view returns(bool isIndeed) {
+        if(userList.length == 0) return false;
+        return (userList[userPublic[_loomAddr].listPointer] == _loomAddr);
+    }
+    
+    // Is user added by NR
+    function isHavenUser(address _naturalRightsId) public view returns(bool isIndeed) {
+        naturalRightsIdHash = uint256(keccak256(_naturalRightsId));
+        return (naturalRightsOwner[naturalRightsIdHash] > 0);
+    }
 
-    event setHavenAccount(address user, string userGunNick);
+    // Number of active users
+    function getUserCount() public view returns(uint userCount) {
+        return userList.length;
+    }
+    
+    function setUserName(string _userName) private {
+        userNameHash = uint256(keccak256(_userName));
+        userNameOwner[newUserNameHash] = msg.sender;
+        userPublic[msg.sender].userName = _userName;
+    }
 
-    function setHavenAccount(string memory _gunNick, ) public {
-
-        //check that the user doesnt exist since before
-        //search by string hash
-        //...
-        if (listOfSocialUsernames[msg.sender] > 5)
-        {
-            //we found that the user already has over 5
-            revert("the user already has a nick");
+    // user initializes the public and private users structs to become active
+    function initUser(string _naturalRightsId, string _userName, string _cryptPubKey, string _encCryptPrivKey, string _encSignPrivKey) public returns(bool success) {
+        if(isHavenUser(_naturalRightsId) {
+            if(isLoomUser(msg.sender)) {
+                revert ("User is already registered")
+            } else {
+                if(userNameOwner[_userName])) {
+                    revert ("Username already taken")
+                } else {
+                userPrivate[msg.sender].cryptPubKey = _cryptPubKey;
+                userPrivate[msg.sender].encCryptPrivKey = _encCryptPrivKey;
+                userPrivate[msg.sender].encSignPrivKey = _encSignPrivKey;
+                userPublic[msg.sender].naturalRightsId = _naturalRightsId
+                userPublic[msg.sender].listPointer = userList.push(msg.sender) - 1;
+                setUserName(_userName);
+                }
+            }
+        } else {
+            revert("Natural Rights account does not exist")
         }
+        return true;
+    }
 
-        //check that any other user doesnt have the same nick since before
-        bytes memory _gunNickInBytes = bytes(_gunNick);
-        uint256 gunNickHash = uint256(keccak256(_gunNickInBytes));
-
-        bytes memory tempEmptyStringTest = bytes(listOfGunNickHashTranslations[gunNickHash]);
-        if (tempEmptyStringTest.length != 0){
-            revert("the name already exists for another nick");
+    function updateUserPriv(string _cryptPubKey, string _encCryptPrivKey, string _encSignPrivKey) public returns(bool success) {
+        if(!isLoomUser(msg.sender)) {
+            revert('User does not exist');
         }
-        //if(keccak256(_gunNick) == keccak256(listOfGunNickHashTranslations[gunNickHash]) ){
-
-        //}
-        //add the user address and nick to the store
-        //listOfGunNicksIndex = listOfGunNicksIndex.length + 1;
-        listOfGunNicks[msg.sender] = gunNickHash;
-        listOfGunNickHashTranslations[gunNickHash] = _gunNick;
-
-        emit SetUserGunNick(msg.sender, _gunNick);
+        userPrivate[msg.sender].cryptPubKey = _cryptPubKey;
+        userPrivate[msg.sender].encCryptPrivKey = _encCryptPrivKey;
+        userPrivate[msg.sender].encSignPrivKey = _encSignPrivKey;
+        return true;
     }
 
-    function getUserGunNick(address _user) public view returns (string memory) {
-
-        return listOfGunNickHashTranslations[listOfGunNicks[_user]];
+    function updateUserName(string newUserName) public returns(bool success) {
+        if(!isLoomUser(msg.sender)) {
+            revert('User does not exist')
+        }
+        if(userNameOwner[_userName])) {
+            revert ("Username already taken")
+        } else {
+        oldUserNameHash = uint256(keccak256(userPublic[msg.sender].userName));
+        delete userNameOwner[oldUserNameHash];
+        setUserName(newUserName);
+        return true
     }
 
-    event SetGunKey(address user, string userGunNick);
-
-    function setGunKey(string memory _gunKey) public {
-        listOfGunEncryptedKeys[msg.sender] = _gunKey;
-
-        emit SetGunKey(msg.sender, _gunKey);
+    function initTransferUsername(address confirmAddr, string expectedUserName) public returns (bool success) {
+        if(!isLoomUser(msg.sender)) {
+            revert('Initiating User does not exist');
+        }
+        if(!isLoomUser(confirmAddr)) {
+            revert('Confirming User does not exist');
+        }
+        if(userPublic[confirmAddr].userName == expectedUserName) {
+            userNameTransferQueue[confirmAddr].initiator = msg.sender;
+            userNameTransferQueue[confirmAddr].listPointer = userNameTransferList.push(confirmAddr) - 1;
+        }
+        return true;
     }
 
-    function getGunKey(address _user) public view returns (string memory) {
-        return listOfGunEncryptedKeys[_user];
+    function confirmTransferUsername(string expectedUserName) public returns(bool success) {
+        address initiator = userNameTransferQueue[msg.sender].initiator;
+        if(!isLoomUser(initiator)) {
+            revert('Initiating User does not exist');
+        }
+        if(!isLoomUser(msg.sender)) {
+            revert('Confirming User does not exist');
+        }
+        if(userPublic[initiator].userName == expectedUserName) {
+            if(userNameTransferQueue[msg.sender].initExpectedUserName == userPublic[msg.sender].userName) {
+                expectedUserNameHash = uint256(keccak256(expectedUserName));
+                confirmUserNameHash = uint256(keccak256(userPublic[msg.sender].userName));
+                userNameOwner[expectedUserNameHash] = initiator;
+                userNameOwner[confirmUserNameHash] = msg.sender;
+                userPublic[initiator].userName = userPublic.[msg.sender].userName;
+                userPublic[msg.sender].userName = expectedUserName;
+                // Remove the userName transfer queue and row in list
+                delete naturalRightsTransferQueue[msg.sender];
+                uint rowToDelete = userNameTransferList[msg.sender].listPointer;
+                address keyToMove = userNameTransferList[userNameTransferList.length-1];
+                userNameTransferList[rowToDelete] = keyToMove;
+                userNameTransferQueue[keyToMove].listPointer = rowToDelete;
+                userNameTransferList.length--;
+            }
+        }
     }
 
+    // deletes the user from the userList and also deletes the userName. userPriv and NR account mapping left for recovery
+    function deleteUser() public returns(bool success) {
+        if(!isLoomUser(msg.sender)) {
+            revert('User does not exist');
+        }
+        uint rowToDelete = userPublic[msg.sender].listPointer;
+        address keyToMove = userList[userList.length-1];
+        userList[rowToDelete] = keyToMove;
+        userPublic[keyToMove].listPointer = rowToDelete;
+        userList.length--;
+        uint256 userNameHash = uint256(keccak256(userPublic[msg.sender].userName))
+        delete userNameOwner[userNameHash]
+        delete userPublic[msg.sender].userName 
+        return true;
+    }
 
+    // Used to recover the natural rights encrypted private keys
+    function recoverUserPriv() public returns(userPriv) {
+        if(!isLoomUser(msg.sender)) {
+            revert('User does not exist')
+        }
+        return userPriv[msg.sender]
+    }
+
+    // Used to recover the user public identifiers
+    function recoverUserPub() public returns(userPub) {
+        if(!isLoomUser(msg.sender)) {
+            revert('User does not exist')
+        }
+        return userPub[msg.sender]
+    }
+    
+    // Reactivate previously deleted user
+    function restoreUser(string userName) return(bool success) {
+        if(isLoomUser(msg.sender)) {
+            revert('User exists')
+        }
+        setUserName(userName);
+        userPublic[msg.sender].listPointer = userList.push(msg.sender) - 1;
+    }
 }
